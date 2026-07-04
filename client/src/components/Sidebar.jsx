103e48f96a2c4d08853e131e850404dee53e19dd
kritikagrover83 <kritikagrover83@gmail.com>
2026-07-04 22:17:27 +0530

    Updated dashboard UI and sidebar styling

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,65 +4,56 @@ import {
   FileText,
   Trash2,
   LogOut,
+  Layers3,
 } from "lucide-react";
 
 function Sidebar({ currentPage, setCurrentPage }) {
   return (
     <aside className="sidebar">
+      {/* Logo */}
       <div className="logo-section">
-        <div className="logo-box">CF</div>
+        <div className="logo-box">
+          <Layers3 size={26} strokeWidth={2.5} color="white" />
+        </div>
 
         <div>
           <h2 className="logo">CourseFlow</h2>
-          <p className="logo-subtitle">
-            AI Learning Platform
-          </p>
+          <p className="logo-subtitle">AI Learning Platform</p>
         </div>
       </div>
 
+      {/* Navigation */}
       <ul className="nav-links">
         <li
-          className={
-            currentPage === "dashboard"
-              ? "active"
-              : ""
-          }
-          onClick={() =>
-            setCurrentPage("dashboard")
-          }
+          className={currentPage === "dashboard" ? "active" : ""}
+          onClick={() => setCurrentPage("dashboard")}
         >
           <LayoutDashboard size={20} />
           <span>Dashboard</span>
         </li>
 
-        <li>
+        <li className={currentPage === "explore" ? "active" : ""}>
           <Compass size={20} />
           <span>Explore</span>
         </li>
 
-        <li>
+        <li className={currentPage === "unpublished" ? "active" : ""}>
           <FileText size={20} />
           <span>Unpublished</span>
         </li>
 
         <li
-          className={
-            currentPage === "delete"
-              ? "active"
-              : ""
-          }
-          onClick={() =>
-            setCurrentPage("delete")
-          }
+          className={currentPage === "delete" ? "active" : ""}
+          onClick={() => setCurrentPage("delete")}
         >
           <Trash2 size={20} />
           <span>Delete Courses</span>
         </li>
 
         <li onClick={() => alert("Logged Out Successfully")}>
-  <LogOut size={20} />
-  <span>Sign Out</span>
-</li>
+          <LogOut size={20} />
+          <span>Sign Out</span>
+        </li>
       </ul>
     </aside>
   );
