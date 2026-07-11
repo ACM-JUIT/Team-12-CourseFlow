df319352dda8bc3eecd8e8bc3c12541e165c500d
Pragnya Gaur <prgnyagaur@gmail.com>
2026-07-11 14:34:12 +0530

    Remove developer name from footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,17 @@
-<footer
-  style={{
-    borderTop: "1px solid #ddd",
-    padding: "40px",
-    marginTop: "60px",
-  }}
->
-  <h3>CourseFlow</h3>
+function Footer() {
+  return (
+    <footer
+      style={{
+        borderTop: "1px solid #ddd",
+        padding: "40px",
+        marginTop: "60px",
+      }}
+    >
+      <h3>CourseFlow</h3>
 
-  <p>© 2026 CourseFlow™. All Rights Reserved.</p>
-</footer>
+      <p>© 2026 CourseFlow™. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
